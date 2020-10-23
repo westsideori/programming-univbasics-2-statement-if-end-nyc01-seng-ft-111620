b1f760f1c13100a6722998677b658a91843d0fee
inventive-view-7245 <ori.markowitz@gmail.com>
2020-10-23 09:10:14 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,8 @@
-run_code_inside = false
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
+current_time = Time.now
+current_time = current_time.to_i
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
 end
-puts "Code after if...end"
+
